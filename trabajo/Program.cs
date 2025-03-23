7b25a4a28b1f70353c01732a85bdb867c179d225
susana2329 <susana.cordon05@gmail.com>
2025-03-22 23:42:55 -0300

    trabajo: crear 2 clases

--- trabajo/Program.cs
+++ trabajo/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-           
+           persona pr1 = new persona();
+            pr1.nombre = "sofia";
+            pr1.edad = 20;
+            pr1.peso=55.4;
+            pr1.profesion = "tecnica en laboratorio";
+            pr1.correr();
+            pr1.ejercer();
+
+
+
+            gato gat = new gato();
+            gat.raza = "siames";
+            gat.nombre1 = "dulce de leche";
+            gat.edad = 3;
+            gat.peso = 5.3;
+            gat.pedircomida();
+            gat.comer();
+            Console.ReadKey();
         }
     }
+    public class persona
+    {
+       public string nombre;
+        public int edad;
+        public double peso;
+        public string profesion;
+
+
+        public void  correr ()  {
+            Console.WriteLine($"{nombre} corre para mantenerse en forma");
+            }
+        public void ejercer  ()
+        {
+            Console.WriteLine($"{nombre} esta ejerciendo como {profesion}");
+        }
+    }
+    public class gato
+    {
+        public string raza;
+        public string nombre1;
+        public int  edad;
+        public double peso;
+        
+        public void pedircomida()
+        {
+            Console.WriteLine($"{nombre1} de raza {raza} esta maullando");
+        }
+        public void comer()
+        {
+            Console.WriteLine($"{nombre1} esta comiendo apesar que pesa {peso} esta gordo");
+        }
+    }
+
 }
